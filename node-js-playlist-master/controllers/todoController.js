d0fe0240f7da6b1daeb97d6046026225a5f06cb4
yisha0307 <chenchen9037@126.com>
2017-04-17 15:28:02 +0800

    todo app using nodejs & mongodb

--- node-js-playlist-master/controllers/todoController.js
+++ node-js-playlist-master/controllers/todoController.js
@@ -12,28 +12,38 @@ var todoSchema = new mongoose.Schema({
 });
 //create a model based on the todoSchema
 var Todo = mongoose.model('Todo',todoSchema);
-var itemOne = Todo({item: 'buy flowers'}).save(function(err){
-	if(err) throw err;
-	console.log('item saved');
-});
+// var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+// 	if(err) throw err;
+// 	console.log('item saved');
+// });
 
 //POST middleware
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-var data =[{item: 'get milk'}, {item: 'walk dog'}, {item:'kick some coding ass'}];
+//var data =[{item: 'get milk'}, {item: 'walk dog'}, {item:'kick some coding ass'}];
 
 module.exports = function(app){
 	app.get('/todo', function(req,res){
-		res.render('todo', {todos: data});
+		//get data from mongodb and pass it to view
+		//要获得all data就用empty obj, 如果要specific one就要在{}里传入那个特定的数据
+		Todo.find({}, function(err,data){
+			if(err) throw err;
+			res.render('todo', {todos:data});
+		});
 	});
 	app.post('/todo', urlencodedParser,function(req,res){
-		data.push(req.body);
-		res.json(data);
+		//get data from the view and add it to mongodb
+		var newTodo = Todo(req.body).save(function(err,data){
+			if(err) throw err;
+			res.json(data);
+		})
 	});
 	app.delete('/todo/:item', function(req,res){
-		data = data.filter(function(todo){
-			return todo.item.replace( / /g, '-') != req.params.item;
-		});
-		res.json(data);
+		//delete the item from mongodb
+		//在url里会有'-'， 所以要用' '来replace
+		Todo.find({item: req.params.item.replace(/\-/g,' ')}).remove(function(err,data){
+			if(err) throw err;
+			res.json(data);
+		})
 	});
 };
