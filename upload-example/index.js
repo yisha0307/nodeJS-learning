dc0ce72498c84d2357cad8d517292f38d8c35510
Yisha Chen <chenchen9037@126.com>
2018-03-01 22:35:27 +0800

    完成了一个上传图表的功能

--- upload-example/index.js
+++ upload-example/index.js
@@ -1,4 +1,11 @@
 var server = require('./server');
 var router = require('./router');
+var requestHandlers = require('./requestHandlers');
 
-server.start(router.route);
+const handlers = {};
+handlers['/'] = requestHandlers.start;
+handlers['/start'] = requestHandlers.start;
+handlers['/upload'] = requestHandlers.upload;
+handlers['/show'] = requestHandlers.show;
+
+server.start(router.route, handlers);
--- upload-example/server.js
+++ upload-example/server.js
@@ -1,18 +1,15 @@
 var http = require('http');
 var url = require('url');
 
-function start(route){
-	http.createServer(function(req,res){
-		var pathname = url.parse(req.url).pathname;
-		console.log('Request for '+ req.url + ' received.');
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log('Request for ' + pathname + ' received.');
+    route(handle, pathname, response, request);
+  }
 
-		route(pathname);
-		
-		res.writeHead(200, {'Content-Type':'text/plain'});
-		res.write('hello world');
-		res.end()
-	}).listen(8888);
-	console.log('Server has started');
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started.');
 }
 
 exports.start = start;
